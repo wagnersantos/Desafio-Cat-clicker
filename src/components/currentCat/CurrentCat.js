77ce71d1bfe497c68bddd5eec745c9c0367019b9
wagnersantos <wagtvc@gmail.com>
2018-12-28 15:05:56 -0200

    adicionando construtor e eventos incremetCouter, resetCounter  em CurrentCat

--- src/components/currentCat/CurrentCat.js
+++ src/components/currentCat/CurrentCat.js
@@ -1,17 +1,32 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 
-class CatView extends Component {
-
-    render() {
-    	const {cats,currentIndex} = this.props;
-        return (
-        	<div className='cat'>
-				<h2>{cats[currentIndex].name}</h2>
-				<p className='cat__count'><strong>{cats[currentIndex].clickCount}</strong></p>
-				<img src={require(`../../assets/${cats[currentIndex].imgSrc}`)} alt='cat' />
-			</div>
-        );
+class CurrentCat extends Component {
+    constructor(props){
+        super(props);
+        this.counter = 0;
+        this.incrementCounter = this.incrementCounter.bind(this);
+        this.resetCounter = this.resetCounter.bind(this);
+    }
+    incrementCounter(){
+        this.resetCounter();
+        this.counter = this.counter +1;
+        this.props.cats[this.props.currentIndex].clickCount = this.counter;
+        this.forceUpdate();
+    }
+    resetCounter(){
+        this.counter = this.props.cats[this.props.currentIndex].clickCount;     
+    }
+    render(){
+        const {cats,currentIndex} = this.props;
+        return (            
+                <div>
+                    <h2>{cats[currentIndex].name}</h2>
+                    <p className='cat__count'><strong>{cats[currentIndex].clickCount}</strong></p>
+                    <img src={require(`../../assets/${cats[currentIndex].imgSrc}`)} alt='cat'
+                     className='shadow-lg img-fluid border border-dark rounded'  onClick={this.incrementCounter}  />
+                </div>
+        )
     }
 }
 
-export default CatView;
+export default CurrentCat;
