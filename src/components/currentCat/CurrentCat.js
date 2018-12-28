683ee777d64b1c89ab22fcdedb212800ba380968
wagnersantos <wagtvc@gmail.com>
2018-12-28 14:38:26 -0200

    adicionando imagens ao component CurrentCat

--- src/components/currentCat/CurrentCat.js
+++ src/components/currentCat/CurrentCat.js
@@ -8,6 +8,7 @@ class CatView extends Component {
         	<div className='cat'>
 				<h2>{cats[0].name}</h2>
 				<p className='cat__count'><strong>{cats[0].clickCount}</strong></p>
+				<img src={require(`../../assets/${cats[0].imgSrc}`)} alt='cat' />
 			</div>
         );
     }
--- src/services/cats/Cats.js
+++ src/services/cats/Cats.js
@@ -2,31 +2,31 @@ const Cats = [
     {
         clickCount : 0,
         name : 'Tabby',
-        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        imgSrc : 'img/Tabby.jpg',
         imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
     },
     {
         clickCount : 0,
         name : 'Tiger',
-        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        imgSrc : 'img/Tiger.jpg',
         imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
     },
     {
         clickCount : 0,
         name : 'Scaredy',
-        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        imgSrc : 'img/Scaredy.jpg',
         imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
     },
     {
         clickCount : 0,
         name : 'Shadow',
-        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        imgSrc : 'img/Shadow.jpg',
         imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
     },
     {
         clickCount : 0,
         name : 'Sleepy',
-        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        imgSrc : 'img/Sleepy.jpg',
         imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
     },
     {
