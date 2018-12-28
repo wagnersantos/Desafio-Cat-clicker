82eb67f8bc66eb0b93d2c5ee583a1cfb3d011ba4
wagnersantos <wagtvc@gmail.com>
2018-12-28 14:46:15 -0200

    adicionando construtor e evento handleClick em CatList

--- src/components/catList/CatList.js
+++ src/components/catList/CatList.js
@@ -1,14 +1,23 @@
 import React,{Component} from 'react';
 
 class CatView extends Component {
+	constructor(props){
+		super(props);
 
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e,index){
+		e.preventDefault();
+		console.log(e.target.innerHTML);
+	}
     render() {
         return (
         	<div>
 	        	<ul>
 					{
 						this.props.cats.map((cat,index) => 
-							<li key={index}>
+							<li key={index}
+								onClick={(e) => this.handleClick(e,index)} >
 								<strong>{cat.name}</strong>
 							</li>
 						)
